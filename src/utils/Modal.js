733ffdeaf829c573fbbef519be55ed9e12b59768
muskacirca <muskacirca@gmail.com>
2016-09-23 14:29:23 +0200

    added modal for subscription

--- src/utils/Modal.js
+++ src/utils/Modal.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Modal from 'react-bootstrap'
+import {Modal} from 'react-bootstrap'
 
 class ModalWrapper extends React.Component {
 
@@ -11,9 +11,19 @@ class ModalWrapper extends React.Component {
         
         return  <div>
                     <div className="static-modal">
-                        <Modal>
-                            Hello
-                        </Modal>
+                        <Modal.Dialog>
+                            <Modal.Header>
+                                <Modal.Title>Modal title</Modal.Title>
+                            </Modal.Header>
+
+                            <Modal.Body>
+                                One fine body...
+                            </Modal.Body>
+                            <Modal.Footer>
+                                <button className="btn btn-default">Close</button>
+                                <button className="btn btn-primary">Save changes</button>
+                            </Modal.Footer>
+                        </Modal.Dialog>
                     </div>
                 </div>
     
