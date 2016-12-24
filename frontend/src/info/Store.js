126864143fee66f66fcc1c1665a2c125c49c14a6
Vincent Truchot <muskacirca@gmail.com>
2016-12-24 13:41:52 +0100

    adding proxy

---
+++ frontend/src/info/Store.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Store extends React.Component {
+    
+    render() {
+        
+        return  <div>
+                    Store
+                </div>
+    
+    }
+}
+
+export default Store
